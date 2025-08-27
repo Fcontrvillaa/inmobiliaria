e7ec1e428f1c73ef81efbd6cd5201fa09afd96dc
Fcontrvillaa <fcontreras+1@gmail.com>
2025-08-27 03:12:06 +0000

    modelos2

--- backend/portal/models.py
+++ backend/portal/models.py
@@ -16,3 +16,27 @@ class Comuna(models.Model):
 
     def __str__(self):
         return f"{self.nombre} --- numero de region es. {self.region}"  #valparaiso --- numero de region es.:  V
+
+
+#modelo inmueble
+
+class Inmueble(models.Model):
+    class Tipo_de_inmueble(models.TextChoices):
+        casa = "CASA", _("Casa")
+        depto = "DEPARTAMENTO", _("Departamento")
+        parcela = "PARCELA", _("Parcela")
+
+
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    m2_construidos = models.FloatField(default=0)
+    m2_totales = models.FloatField(default=0)
+    estacionamientos = models.PositiveSmallIntegerField(default=0)
+    habitaciones = models.PositiveSmallIntegerField(default=0)
+    baños = models.PositiveSmallIntegerField(default=0)
+    dirección = models.CharField(max_length=100)
+    precio_mensual = models.DecimalField(max_digits=8, decimal_places=2)
+    creado = models.DateTimeField(auto_now_add=True)
+    modificado = models.DateTimeField(auto_now=True)
+    comuna = models.ForeignKey(Comuna, on_delete=models.PROTECT, related_name="inmuebles")
+
